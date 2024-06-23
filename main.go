c8dd17f999b4091f4fecc96df85afe639cdf093e
renato@likwi.com.br <renatofagalde>
2024-06-23 10:38:39 -0700

    initial files

--- main.go
+++ main.go
@@ -1,54 +1,44 @@
-#!/bin/bash
+package main
 
-# Função para validar o domínio
-validate_domain() {
-    if [[ ! "$1" =~ ^[a-zA-Z]+$ ]]; then
-        echo "Domínio inválido. O domínio deve conter apenas letras e não pode ter números, espaços ou caracteres especiais."
-        exit 1
-    fi
+import (
+	"context"
+	"fmt"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	toolkit "github.com/renatofagalde/golang-toolkit"
+	"log"
+	postgres "main/src/config/database/postgres/gorm"
+	"main/src/controller/routes"
+)
+
+func init() {
+
 }
 
-# Solicitar o domínio
-read -p "Digite o domínio: " DOMAIN
+func main() {
 
-# Validar o domínio
-validate_domain "$DOMAIN"
+	var tools toolkit.Tools
+	var logger toolkit.Logger
+	config, err := tools.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot read the env file")
+	}
 
-# Converter o domínio para minúsculas
-DOMAIN_LOWER=$(echo "$DOMAIN" | tr '[:upper:]' '[:lower:]')
-# Converter a primeira letra para maiúscula
-DOMAIN_CAPITALIZED=$(echo "$DOMAIN_LOWER" | sed 's/.*/\u&/')
+	database, err := postgres.NewPostgresGORMConnection(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatalf("Error ao conectar no no banco, error=%s", err.Error())
+		return
+	} else {
+		fmt.Println("conexao com sucesso")
+	}
 
-# Função para renomear arquivos e diretórios
-rename_files_and_directories() {
-    find . -name '*CUSTOM*' | while read -r file; do
-        newfile=$(echo "$file" | sed "s/CUSTOM/$DOMAIN_LOWER/g")
-        mv "$file" "$newfile"
-    done
-
-    # Renomear novamente para capturar subdiretórios e arquivos já renomeados
-    find . -name '*CUSTOM*' | while read -r file; do
-        newfile=$(echo "$file" | sed "s/CUSTOM/$DOMAIN_LOWER/g")
-        mv "$file" "$newfile"
-    done
+	userController := initDependencies(database)
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.Use(cors.Default())
+	routes.InitRoutes(&router.RouterGroup, userController)
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+	logger.Info("Iniciando")
 }
-
-# Função para substituir ocorrências dentro dos arquivos
-replace_within_files() {
-    find . -type f -not -name "setup.sh" -print0 | while IFS= read -r -d '' file; do
-        sed -i "s/\bCUSTOM\b/$DOMAIN/g" "$file"
-        sed -i "s/\bCustom\b/$DOMAIN_CAPITALIZED/g" "$file"
-        sed -i "s/\bcustom\b/$DOMAIN_LOWER/g" "$file"
-        sed -i "s/CUSTOM/$DOMAIN/g" "$file"
-        sed -i "s/Custom/$DOMAIN_CAPITALIZED/g" "$file"
-        sed -i "s/custom/$DOMAIN_LOWER/g" "$file"
-    done
-}
-
-# Renomear arquivos e diretórios
-rename_files_and_directories
-
-# Substituir ocorrências dentro dos arquivos
-replace_within_files
-
-echo "Configuração concluída com sucesso!"
